7f568b01ff678123929ed8e0808bbdcf25297816
wyxiao6 <71328511+davidhsiaotw@users.noreply.github.com>
2024-03-05 19:10:52 +0800

    build: initialize project

---
+++ src/test/java/com/esun/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esun.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
